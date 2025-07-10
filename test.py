9442267c0aaa9ed39366e9940318050f3bc9cbd3
afinner <158837178+afinner@users.noreply.github.com>
2025-07-10 11:26:56 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load with low_memory=False to avoid warnings
-df = pd.read_csv('stemma_extra_fields.csv', low_memory=False)
-
-# Show the list of columns
-print(df.columns.tolist())
